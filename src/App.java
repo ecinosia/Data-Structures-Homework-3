a927142bf1681e16a0b5165520efb4c2976d532a
ecinosia <furkansaridiken@gmail.com>
2023-06-09 16:56:30 +0300

    FINAL COMMIT

--- src/App.java
+++ src/App.java
@@ -1,11 +1,19 @@
+//TAHIR FURKAN SARIDIKEN
+//200316004
+//DATA STRUCTURES HOMEWORK
+//COMPUTER ENGINEERING 
+//SECOND GRADE
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
+        //Creating the tree for operations using BinarySearchTree function from QuestionOne.java file.
         BinarySearchTree tree = new BinarySearchTree();
 
+        //Inserting roots of the tree.
         tree.insert(70);
         tree.insert(45);
         tree.insert(87);
@@ -26,10 +34,48 @@ public class App {
         tree.insert(79);
         tree.insert(82);
 
-        //ArrayList<Integer> preorderArray = tree.preorder();
+        //Calling the preorder function and hold it in ArrayList named preorderArray.
+        ArrayList<Integer> preorderArray = tree.preorder();
 
+        //We convert the preorderArray ArrayList to an Array and convert it to String to print it.
+        System.out.println("Preorder Output: " + Arrays.toString(preorderArray.toArray()) + "\n");
         
-        
+        //Initiliaze the keys for the Lowest Common Ancestor operation.
+        int key1 = 27;
+        int key2 = 46;
+        int key3 = 77;
+        int key4 = 13;
+        //Use keys for inputs and call the findLowestCommonAncestor function. 
+        //Lowest common ancestors are nodes. With this we can easily print it out in the next lines.
+        Node lowestCommonAncestor = tree.findLowestCommonAncestor(key1, key2);
+        Node lowestCommonAncestor2 = tree.findLowestCommonAncestor(key3, key4);
+        //If the function finds the lowest common ancestor it will be printed with this.
+        if (lowestCommonAncestor != null) {
+            System.out.println("Lowest Common Ancestor of " + key1 + " and " + key2 + ": " + lowestCommonAncestor.key + "\n");
+        } else { // If not not found error will be printed.
+            System.out.println("Lowest Common Ancestor not found.");
+        }
 
+        if (lowestCommonAncestor != null) {
+            System.out.println("Lowest Common Ancestor of " + key3 + " and " + key4 + ": " + lowestCommonAncestor2.key + "\n");
+        } else {
+            System.out.println("Lowest Common Ancestor not found.");
+        }
+        //Initialize to key to delete
+        int keyToDelete = 32;
+        //Call the delete function to delete specific node.
+        tree.delete(keyToDelete);
+        //Printing the key which deleted.
+        System.out.println("Node with key " + keyToDelete + " deleted.\n");
+
+        //Printing the preorder traversal after deleting the node.
+        ArrayList<Integer> preorderArray2 = tree.preorder();
+        System.out.println("New Preorder Output: " + Arrays.toString(preorderArray2.toArray()) + "\n");
+
+        //We create an ArrayList named levelOrderArray to hold levelOrdered tree.
+        //And we call the levelOrder function for this.
+        ArrayList<Integer> levelOrderArray = tree.levelOrder();
+        //Printing the Level Order Traversal Output with same steps as we did in PreOrder Traversal Output.
+        System.out.println("Level Order Traversal Output: " + Arrays.toString(levelOrderArray.toArray()));
     }
 }
--- src/QuestionOne.java
+++ src/QuestionOne.java
@@ -1,62 +1,153 @@
+//TAHIR FURKAN SARIDIKEN
+//200316004
+//DATA STRUCTURES HOMEWORK
+//COMPUTER ENGINEERING 
+//SECOND GRADE
+
 import java.util.ArrayList;
-
-class Node {
-    int key;
-    Node left, right;
-    
-    public Node(int item) {
-        key = item;
-        left = right = null;
-    }
-}
-
-class BinarySearchTree {
-    Node root;
-
-    BinarySearchTree() {
-        root = null;
-    }
-    
-    void insert(int key) {
-        root = insertRec(root, key);
-    }
-    
-    Node insertRec(Node root, int key) {
-        if (root == null) {
-            root = new Node(key);
-            return root;
-        }
-        
-        if (key < root.key)
-            root.left = insertRec(root.left, key);
-        else if (key > root.key)
-            root.right = insertRec(root.right, key);
-        
-        return root;
-    }
-
-   
-
-    ArrayList<Integer> preorder() {
-        ArrayList<Integer> result = new ArrayList<>();
-        preorderRec(root, result);
-        return result;
-    }
-
-    void preorderRec(Node root, ArrayList<Integer> result) {
-        if (root != null) {
-            result.add(root.key);
-            preorderRec(root.left, result);
-            preorderRec(root.right, result);
-        }
-    }
-}
+import java.util.LinkedList;
+import java.util.Queue;
 
 
-
-public class QuestionOne {
-    
-    
-        
+        //Creating the Node class to create our binary search tree.
+        class Node {
+            int key;
+            Node left, right;
             
-}
+            public Node(int item) {
+                key = item;
+                left = right = null;
+            }
+        }
+        //Creating BinarySearchTree class.
+        class BinarySearchTree {
+            //Create the root with using Node class.
+            Node root;
+    
+            //Initiliaze the root as null.
+            BinarySearchTree() {
+                root = null;
+            }
+            
+            //Creating insert function to create our binary search tree with user input.
+            void insert(int key) {
+                root = insertRec(root, key);
+            }
+            
+            //Check the roots and create Nodes for the tree.
+            Node insertRec(Node root, int key) {
+                if (root == null) {
+                    root = new Node(key);
+                    return root;
+                }
+                //Compare the values and put the Nodes in to their places recording to that comparing.
+                if (key < root.key)
+                    root.left = insertRec(root.left, key);
+                else if (key > root.key)
+                    root.right = insertRec(root.right, key);
+                
+                return root;
+            }
+    
+        
+            //Creating an ArrayList to insert PreOrdered tree.
+            ArrayList<Integer> preorder() {
+                ArrayList<Integer> result = new ArrayList<>();
+                preorderRec(root, result);
+                return result;
+            }
+    
+            //Creating this function for Preorder Traversal.
+            void preorderRec(Node root, ArrayList<Integer> result) {
+                //If statement check if the root is null or not.
+                //The preorderRec method recursively traverses the tree in the order of root, left subtree, right subtree.
+                if (root != null) {
+                    result.add(root.key);
+                    preorderRec(root.left, result);
+                    preorderRec(root.right, result);
+                }
+            }
+    
+            //This method takes two keys key1 and key2 as input and finds the lowest common ancestor of the nodes with those keys.
+            //It recursively traverses the tree by comparing the keys with the
+            //root node and moves left or right accordingly until it finds the lowest common ancestor.
+            Node findLowestCommonAncestor(int key1, int key2) {
+                return findLowestCommonAncestor(root, key1, key2);
+            }
+        
+            Node findLowestCommonAncestor(Node root, int key1, int key2) {
+                if (root == null)
+                    return null;
+        
+                if (root.key > key1 && root.key > key2)
+                    return findLowestCommonAncestor(root.left, key1, key2);
+                else if (root.key < key1 && root.key < key2)
+                    return findLowestCommonAncestor(root.right, key1, key2);
+                else
+                    return root;
+            }
+    
+            //This method takes key as input and delete it with the help of deleteRec function.
+            void delete(int key) {
+                root = deleteRec(root, key);
+            }
+            //It recursively traverses the tree to find the node to delete. Once the node is found, it handles three cases:
+            Node deleteRec(Node root, int key) {
+                //Wanted root is empty.
+                if (root == null)
+                    return root;
+                //If the node to delete has no children, it simply removes the node.
+                //If the node to delete has one child, it replaces the node with its child.
+                if (key < root.key)
+                    root.left = deleteRec(root.left, key);
+                else if (key > root.key)
+                    root.right = deleteRec(root.right, key);
+                else {
+                    // Node to delete found
+                    if (root.left == null)
+                        return root.right;
+                    else if (root.right == null)
+                        return root.left;
+        
+                    // Node to delete has two children
+                    root.key = minValue(root.right);
+                    root.right = deleteRec(root.right, root.key);
+                }
+        
+                return root;
+            }
+        
+            //We use this function to handle third case.
+            int minValue(Node root) {
+                int minValue = root.key;
+                while (root.left != null) {
+                    minValue = root.left.key;
+                    root = root.left;
+                }
+                return minValue;
+            }
+    
+            //This method performs a level order traversal of the tree and returns the result as an ArrayList<Integer>.
+            ArrayList<Integer> levelOrder() {
+                ArrayList<Integer> result = new ArrayList<>();
+                if (root == null)
+                    return result;
+                //It uses a Queue to keep track of the nodes at each level.
+                Queue<Node> queue = new LinkedList<>();
+                queue.add(root);
+                //The key of each visited node is added to the result list.
+                while (!queue.isEmpty()) {
+                    Node node = queue.poll();
+                    result.add(node.key);
+        
+                    if (node.left != null)
+                        queue.add(node.left);
+                    if (node.right != null)
+                        queue.add(node.right);
+                }
+        
+                return result;
+            }
+        }
+                
+
